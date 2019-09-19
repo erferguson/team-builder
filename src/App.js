562c71d623d3510a473a4ca4fc3d57befc2efe25
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-09-18 19:53:21 -0700

    TEAM-BUILDER -- ds.v

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
+import Member from './components/Form';
+import Form from './components/Form';
+
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState ([]);
+
+  const submitMember = member => setMembers([...members, member]);
+  const update = uMember => 
+    setMembers([
+      ...members.map(member => {
+        if (member.id === uMember.id){
+          return uMember;
+        }
+        return member;
+      })
+    ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {members.map((member, i)=> (
+        <Member member={member} key={i} update={update} />
+      ))}
+      <Form add={submitMember} />
     </div>
   );
-}
+  }
 
 export default App;
